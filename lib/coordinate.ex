3bf34371114c76b20a4e85f35d4ff1de61ab7234
Jose Carlo Macariola <jmacariola@stratpoint.com>
2017-06-09 11:34:52 +0800

    updated from .3

--- lib/coordinate.ex
+++ lib/coordinate.ex
@@ -1,41 +1,13 @@
 defmodule IslandsEngine.Coordinate do
-	defstruct in_island: :none, guessed?: false
+	@enforce_keys [:row, :col]
+	defstruct [:row, :col]
+
+	@board_range 1..10
 
 	alias IslandsEngine.Coordinate
 
-	def start_link() do
-		Agent.start_link(fn -> %Coordinate{} end)
-	end
-
-	def guess(coordinate) do
-		Agent.update(coordinate, fn state -> Map.put(state, :guessed?, true) end)
-	end
-
-	def guessed?(coordinate) do
-		Agent.get(coordinate, fn state -> state.guessed? end)
-	end
-
-	def island(coordinate) do
-		Agent.get(coordinate, fn state -> state.in_island end)
-	end
-
-	def in_island?(coordinate) do
-		case island(coordinate) do
-			:none -> false
-			_-> true
-		end
-	end
-
-	def hit?(coordinate) do
-		in_island?(coordinate) && guessed?(coordinate)
-	end
-
-	def set_in_island(coordinate, value) when is_atom value do
-		Agent.update(coordinate, fn state -> Map.put(state, :in_island, value) end)
-	end
-
-	def to_string(coordinate) do
-		"(in_island:#{island(coordinate)}, guessed:#{guessed?(coordinate)})"
-	end
+	def new(row, col) when row in(@board_range) and col in(@board_range), do:
+		{:ok, %Coordinate{row: row, col: col}}
+	def new(_row, _col), do: {:error, :invalid_coordinate}
 
 end
--- lib/island.ex
+++ lib/island.ex
@@ -1,28 +1,66 @@
 defmodule IslandsEngine.Island do
-  alias IslandsEngine.Coordinate
+  alias IslandsEngine.{Coordinate, Island}
+  @enforce_keys [:coordinates, :hit_coordinates]
+  defstruct [:coordinates, :hit_coordinates]
+  def new(), do:
+  %Island{coordinates: MapSet.new(), hit_coordinates: MapSet.new()}
 
-  def start_link() do
-    Agent.start_link(fn -> [] end)
+  defp offsets(:square),
+  do: [{0, 0}, {0, 1}, {1, 0}, {1, 1}]
+
+  defp offsets(:atoll),
+  do: [{0, 0}, {0, 1}, {1, 1}, {2, 0}, {2, 1}]
+
+  defp offsets(:dot),
+  do: [{0, 0}]
+
+  defp offsets(:l_shape),
+  do: [{0, 0}, {1, 0}, {2, 0}, {2, 1}]
+
+  defp offsets(:s_shape),
+  do: [{0, 1}, {0, 2}, {1, 0}, {1, 1}]
+
+  defp offsets(_),
+  do: {:error, :invalid_island_type}
+
+  defp add_coordinates(offsets, upper_left) do
+    Enum.reduce_while(offsets, MapSet.new(), fn offset, acc ->
+    add_coordinate(acc, upper_left, offset)
+    end)
   end
 
-  def replace_coordinates(island, new_coordinates) when is_list new_coordinates do
-    Agent.update(island, fn _state -> new_coordinates end)
+  defp add_coordinate(coordinates, %Coordinate{row: row, col: col},
+    {row_offset, col_offset}) do
+      case Coordinate.new(row + row_offset, col + col_offset) do
+        {:ok, coordinate}
+        -> {:cont, MapSet.put(coordinates, coordinate)}
+        {:error, :invalid_coordinate}
+        -> {:halt, {:error, :invalid_coordinate}}
+      end
   end
 
-  def forested?(island) do
-    island
-    |> Agent.get(fn state -> state end)
-    |> Enum.all?(fn coord -> Coordinate.hit?(coord) end)
+  def new(type, %Coordinate{} = upper_left) do
+    with [_|_] = offsets <- offsets(type),
+    %MapSet{} = coordinates <- add_coordinates(offsets, upper_left)
+    do
+    {:ok, %Island{coordinates: coordinates, hit_coordinates: MapSet.new()}}
+    else
+    error -> error
+    end
   end
 
-  def to_string(island) do
-    "[" <> coordinate_strings(island) <> "]"
+  def overlaps?(existing_island, new_island), do:
+  not MapSet.disjoint?(existing_island.coordinates, new_island.coordinates)
+
+  def guess(island, coordinate) do
+    case MapSet.member?(island.coordinates, coordinate) do
+    true ->
+      hit_coordinates = MapSet.put(island.hit_coordinates, coordinate)
+      {:hit, %{island | hit_coordinates: hit_coordinates}}
+    false -> :miss
+    end
   end
 
-  defp coordinate_strings(island) do
-    island
-    |> Agent.get(fn state -> state end)
-    |> Enum.map(fn coord -> Coordinate.to_string(coord) end)
-    |> Enum.join(", ")
-  end
+  def forested?(island), do:
+    MapSet.equal?(island.coordinates, island.hit_coordinates)
 end
